8ab0658e4a3e44319d2415fc18aa5614e57a9373
ganesh <bejjankiganesh1432@gmail.com>
2024-05-04 04:41:00 +0000

    solution

---
+++ src/main/java/com/example/nxtstayz/repository/HotelRepository.java
@@ -0,0 +1,27 @@
+package com.example.nxtstayz.repository;
+
+
+import com.example.nxtstayz.model.Hotel;
+import com.example.nxtstayz.model.Room;
+import java.util.List;
+import java.util.ArrayList;
+
+
+public interface HotelRepository {
+    ArrayList<Hotel> getHotels();
+
+
+    Hotel getHotelById(int hotelId);
+
+
+    Hotel addHotel(Hotel hotel);
+
+
+    Hotel updateHotel(int hotelId, Hotel hotel);
+
+
+    void deleteHotel(int hotelId);
+
+
+    List<Room> getHotelRooms(int hotelId);
+}
